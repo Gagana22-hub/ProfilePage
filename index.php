a773097bd288ffe271cea8214a93420df4286af3
Gagana22-hub <86415416+Gagana22-hub@users.noreply.github.com>
2022-09-22 20:08:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  <nav>
   <ul>
    <li><a href="https://FresherProfiles.com">Profiles</a></li>
-   <li><a href="https://fresherprofiles.com/web/student">Profile Builder</a></li>
+   <li><a href="https://fresherprofiles.com/web/student">Profile Builder!!</a></li>
   </ul>
  </nav>
 </header>
